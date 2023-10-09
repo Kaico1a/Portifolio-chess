480371bee31d1e5f1476b7b856be09b584289cc0
Kaicola <kaiquepenna@gmail.com>
2023-10-09 17:06:07 -0300

    criando classes das peças + aplicando movimentos especiais + fim do programa

--- chess/src/program/Main.java
+++ chess/src/program/Main.java
@@ -1,4 +1,5 @@
 package program;
+
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -8,7 +9,6 @@ import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
-import program.UI;
 
 public class Main {
 
@@ -17,6 +17,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
+
         while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
@@ -27,7 +28,7 @@ public class Main {
 
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
-                UI.printBoard(ChessMatch.getPieces(), possibleMoves);
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
@@ -38,10 +39,21 @@ public class Main {
                     captured.add(capturedPiece);
                 }
 
-            } catch (ChessException e) {
+                if (chessMatch.getPromoted() != null) {
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+                        System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
+                    chessMatch.replacePromotedPiece(type);
+                }
+            }
+            catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
-            } catch (InputMismatchException e) {
+            }
+            catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
